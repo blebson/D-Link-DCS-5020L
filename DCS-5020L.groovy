8c3019f7f5a655f6788016c53b92df26a7835709
Ben Lebson <blebson@gmail.com>
2016-04-22 20:33:43 -0700

    New UI, Added Video Streaming

--- DCS-5020L.groovy
+++ DCS-5020L.groovy
@@ -1,9 +1,8 @@
 /**
- *	D-Link DCS-5020L v1.1.3
- *  Modified from Generic Camera Device v1.0.07102014
- *
- *  Copyright 2014 patrick@patrickstuart.com
- *  Modified 2015 blebson
+ *	D-Link DCS-5020L v2.0.0
+ *  Image Capture and Video Streaming courtesy Patrick Stuart (patrick@patrickstuart.com)
+ *  
+ *  Copyright 2015 blebson
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -16,42 +15,89 @@
  *
  */
 metadata {
-	definition (name: "DCS-5020L", author: "blebson") {
+	definition (name: "DCS-5020L-Test", author: "blebson") {
 		capability "Image Capture"
 		capability "Sensor"
 		capability "Switch"
         capability "Switch Level"
         capability "Refresh"
         capability "Notification"
+        capability "Configuration"
+		capability "Video Camera"
+		capability "Video Capture"
         
 		attribute "hubactionMode", "string"
         attribute "position", "string"
+        attribute "switch2", "string"
         
         command "left"
 		command "right"
 		command "up"
 		command "down"
         command "home"
-        command "preset"
+        command "presetOne"
+        command "presetTwo"
+        command "presetThree"
         command "nvOn"
         command "nvOff"
         command "nvAuto"
         command "presetCommand"
+        command "start"
+		command "stop"
+        command "vidOn"
+        command "vidOff"
 	}
 
     preferences {
     input("CameraIP", "string", title:"Camera IP Address", description: "Please enter your camera's IP Address", required: true, displayDuringSetup: true)
-    input("CameraPort", "string", title:"Camera Port", description: "Please enter your camera's Port", defaultValue: 80 , required: true, displayDuringSetup: true)
+    input("CameraPort", "string", title:"Camera Port", description: "Please enter your camera's HTTP Port", defaultValue: 80 , required: true, displayDuringSetup: true)
+    input("VideoIP", "string", title:"Video IP Address", description: "Please enter your camera's IP Address (use external IP if you are using port forwarding)", required: true, displayDuringSetup: true)
+    input("VideoPort", "string", title:"Video Port", description: "Please enter your camera's HTTP Port (use external Port if you are using port forwarding)", defaultValue: 80 , required: true, displayDuringSetup: true)
     input("CameraUser", "string", title:"Camera User", description: "Please enter your camera's username", required: false, displayDuringSetup: true)
     input("CameraPassword", "password", title:"Camera Password", description: "Please enter your camera's password", required: false, displayDuringSetup: true)
-    input("CameraPreset", "string", title:"Camera Preset", description: "Please enter which preset view you want to use (other than 'Home')", defaultValue: 1 , required: true, displayDuringSetup: true)
+    input("CameraPresetOne", "string", title:"Camera Preset 1", description: "Please enter which preset view you want to use (other than 'Home')", defaultValue: 1 , required: true, displayDuringSetup: true)
+	input("CameraPresetTwo", "string", title:"Camera Preset 2", description: "Please enter which preset view you want to use (other than 'Home')", defaultValue: 2 , required: false, displayDuringSetup: true)
+    input("CameraPresetThree", "string", title:"Camera Preset 3", description: "Please enter which preset view you want to use (other than 'Home')", defaultValue: 3 , required: false, displayDuringSetup: true)
+    }
+    
+    mappings {
+   		path("/getInHomeURL") {
+       		action:
+       		[GET: "getInHomeURL"]
+   		}
 	}
     
 	simulator {
     
 	}
 
-    tiles {
+    tiles (scale: 2){
+    multiAttributeTile(name: "videoPlayer", type: "videoPlayer", width: 6, height: 4) {
+			tileAttribute("device.switch2", key: "CAMERA_STATUS") {
+				attributeState("on", label: "Active", icon: "st.camera.dlink-indoor", action: "vidOff", backgroundColor: "#79b821", defaultState: true)
+				attributeState("off", label: "Inactive", icon: "st.camera.dlink-indoor", action: "vidOn", backgroundColor: "#ffffff")
+				attributeState("restarting", label: "Connecting", icon: "st.camera.dlink-indoor", backgroundColor: "#53a7c0")
+				attributeState("unavailable", label: "Unavailable", icon: "st.camera.dlink-indoor", action: "refresh.refresh", backgroundColor: "#F22000")
+			}
+
+			tileAttribute("device.camera", key: "PRIMARY_CONTROL") {
+				attributeState("on", label: "Active", icon: "st.camera.dlink-indoor", backgroundColor: "#79b821", defaultState: true)
+				attributeState("off", label: "Inactive", icon: "st.camera.dlink-indoor", backgroundColor: "#ffffff")
+				attributeState("restarting", label: "Connecting", icon: "st.camera.dlink-indoor", backgroundColor: "#53a7c0")
+				attributeState("unavailable", label: "Unavailable", icon: "st.camera.dlink-indoor", backgroundColor: "#F22000")
+			}
+
+			tileAttribute("device.startLive", key: "START_LIVE") {
+				attributeState("live", action: "start", defaultState: true)
+			}
+
+			tileAttribute("device.stream", key: "STREAM_URL") {
+				attributeState("activeURL", defaultState: true)
+			}
+			tileAttribute("device.betaLogo", key: "BETA_LOGO") {
+				attributeState("betaLogo", label: "", value: "", defaultState: true)
+			}
+            }
     	carouselTile("cameraDetails", "device.image", width: 3, height: 2) { }
         standardTile("take", "device.image", width: 1, height: 1, canChangeIcon: false, inactiveLabel: true, canChangeBackground: false) {
             state "take", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
@@ -59,47 +105,67 @@ metadata {
             state "image", label: "Take", action: "Image Capture.take", icon: "st.camera.camera", backgroundColor: "#FFFFFF", nextState:"taking"
         }
         standardTile("up", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
-			state "up", label: "", action: "up", icon: "st.samsung.da.oven_ic_up"
+			state "up", label: "", action: "up", icon: "st.samsung.da.oven_ic_up", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0", icon: "st.samsung.da.oven_ic_up"
 		}
          standardTile("left", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
-			state "left", label: "", action: "left", icon: "st.samsung.da.RAC_4line_01_ic_left"
+			state "left", label: "", action: "left", icon: "st.samsung.da.RAC_4line_01_ic_left", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0", icon: "st.samsung.da.RAC_4line_01_ic_left"
 		}
           standardTile("home", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false) {
-			state "home", label: "Home", action: "home", icon: "st.Home.home2"
+			state "home", label: "Home", action: "home", icon: "st.Home.home2", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0", icon: "st.Home.home2"
 		}
           standardTile("right", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
-			state "right", label: "", action: "right", icon: "st.samsung.da.RAC_4line_03_ic_right"
+			state "right", label: "", action: "right", icon: "st.samsung.da.RAC_4line_03_ic_right", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0", icon: "st.samsung.da.RAC_4line_03_ic_right"
 		}
          standardTile("down", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false, decoration: "flat") {
-			state "down", label: "", action: "down", icon: "st.samsung.da.oven_ic_down"
+			state "down", label: "", action: "down", icon: "st.samsung.da.oven_ic_down", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0", icon: "st.samsung.da.oven_ic_down"
 		}
-        standardTile("preset", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false) {
-			state "preset", label: "Preset", action: "preset", icon: "st.camera.dlink-hdpan"
+        valueTile("presetOne", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false) {
+			state "presetOne", label: "1", action: "presetOne", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0"
+		}
+        valueTile("presetTwo", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false) {
+			state "presetTwo", label: "2", action: "presetTwo", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0"
+		}
+        valueTile("presetThree", "device.image", width: 1, height: 1, canChangeIcon: false,  canChangeBackground: false) {
+			state "presetThree", label: "3", action: "presetThree", nextState: "moving"
+            state "moving", label: "moving", action:"", backgroundColor: "#53a7c0"
 		}
 
         standardTile("refresh", "command.refresh", inactiveLabel: false) {
         	state "default", label:'refresh', action:"refresh.refresh", icon:"st.secondary.refresh-icon"        
     	}
         
-        standardTile("motion", "device.switch", width: 1, height: 1, canChangeIcon: false) {
+        standardTile("motion", "device.switch", width: 2, height: 2, canChangeIcon: false) {
 			state "off", label: 'Motion Off', action: "switch.on", icon: "st.motion.motion.inactive", backgroundColor: "#ccffcc", nextState: "on"
             state "on", label: 'Motion On', action: "switch.off", icon: "st.motion.motion.active", backgroundColor: "#EE0000", nextState: "off"            
 		}
-        controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 1, inactiveLabel: false, range:"(0..100)") {
+        controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 5, inactiveLabel: false, range:"(0..100)") {
             state "level", label: "Motion Detection Sensitivity", action:"switch level.setLevel"
         }
         valueTile("Sensitivity", "device.level", inactiveLabel: false){
         	state "default", label:'${currentValue}%', unit:"%"
         }
-         standardTile("nightVision", "device.switch3", width: 1, height: 1, canChangeIcon: false) {
-			state "off", label: 'Night Vision Off', action: "nvAuto", icon: "st.Weather.weather14", backgroundColor: "#ffff00", nextState: "toggle"
+         standardTile("nightVision", "device.switch3", width: 2, height: 2, canChangeIcon: false) {
+			state "off", label: 'NV Off', action: "nvAuto", icon: "st.Weather.weather14", backgroundColor: "#ffff00", nextState: "toggle"
             state "toggle", label:'toggle', action: "", icon: "st.motion.motion.inactive", backgroundColor: "#53a7c0"
-			state "on", label: 'Night Vision On', action: "nvOff", icon: "st.Weather.weather4", backgroundColor: "#4169E1", nextState: "toggle"  
-            state "auto", label: 'Night Vision Auto', action: "nvOn", icon: "st.motion.motion.active", backgroundColor: "#ccffcc", nextState: "toggle"  
+			state "on", label: 'NV On', action: "nvOff", icon: "st.Weather.weather4", backgroundColor: "#4169E1", nextState: "toggle"  
+            state "auto", label: 'NV Auto', action: "nvOn", icon: "st.motion.motion.active", backgroundColor: "#ccffcc", nextState: "toggle"  
 		}
+        standardTile("videoStart", "device.image", inactiveLabel: false) {
+        	state "start", action:"start", icon:"st.Entertainment.entertainment11"        
+    	}
+        valueTile("blank", "device.image"){
+        	state "blank"
+        }
                  
         main "motion"
-        details(["cameraDetails", "take", "up", "motion", "left", "home", "right", "preset", "down", "refresh", "nightVision", "levelSliderControl", "Sensitivity"])
+        details(["videoPlayer", "presetOne", "presetTwo", "presetThree", "refresh", "up", "videoStart", "cameraDetails", "left", "home", "right", "take", "down", "blank",  "levelSliderControl", "Sensitivity", "nightVision", "motion"])
     }
 }
 
@@ -169,12 +235,21 @@ def parse(String description) {
         else if (msg.body.contains("DayNightMode=0")) {
             log.debug "Night Vision is auto"
             sendEvent(name: "switch3", value: "auto");
-        }
-    }
+        }                   
+    }        
 }
 catch (Exception e) { //needed to catch java.lang.ArrayIndexOutOfBoundsException when camera doesn't reply with anthing in the body
     	//log.debug "Hit Exception $e in Parse"
     }
+    device.deviceNetworkId = "ID_WILL_BE_CHANGED_AT_RUNTIME_" + (Math.abs(new Random().nextInt()) % 99999 + 1)
+    sendEvent(name: "image", value: "down");
+    sendEvent(name: "image", value: "up");
+    sendEvent(name: "image", value: "left");
+    sendEvent(name: "image", value: "right");
+    sendEvent(name: "image", value: "presetOne");
+    sendEvent(name: "image", value: "presetTwo");
+    sendEvent(name: "image", value: "presetThree");
+    sendEvent(name: "image", value: "home");
 }
 
 // handle commands
@@ -416,13 +491,13 @@ def off() {
 }
 def nvOn() {
 	log.debug "Enabling Night Vision"
-    return nightCmd("2")   
+    return nightCmd("3")   
     
 }
 
 def nvOff() {
 	log.debug "Disabling Night Vision"
-    return nightCmd("3")    
+    return nightCmd("2")    
     
 }
 
@@ -582,10 +657,20 @@ def home()
     moveCmd(4)
 }
 
-def preset()
+def presetOne()
 {
 	log.debug "Moving to Preset position"
-    presetCmd(CameraPreset.toInteger())
+    presetCmd(CameraPresetOne.toInteger())
+}
+def presetTwo()
+{
+	log.debug "Moving to Preset position"
+    presetCmd(CameraPresetTwo.toInteger())
+}
+def presetThree()
+{
+	log.debug "Moving to Preset position"
+    presetCmd(CameraPresetThree.toInteger())
 }
 
 def setLevel(percent) {
@@ -595,4 +680,55 @@ def setLevel(percent) {
 
 def presetCommand(position) {
 	presetCmd(position.toInteger())
+}
+
+def start() {
+	log.trace "start()"
+	def dataLiveVideo = [
+		OutHomeURL  : "http://${CameraUser}:${CameraPassword}@${VideoIP}:${VideoPort}/mjpeg.cgi?channel=1.mjpeg",
+		InHomeURL   : "http://${CameraUser}:${CameraPassword}@${VideoIP}:${VideoPort}/mjpeg.cgi?channel=1.mjpeg",
+		ThumbnailURL: "http://cdn.device-icons.smartthings.com/camera/dlink-indoor@2x.png",
+		cookie      : [key: "key", value: "value"]
+	]
+
+	def event = [
+		name           : "stream",
+		value          : groovy.json.JsonOutput.toJson(dataLiveVideo).toString(),
+		data		   : groovy.json.JsonOutput.toJson(dataLiveVideo),
+		descriptionText: "Starting the livestream",
+		eventType      : "VIDEO",
+		displayed      : false,
+		isStateChange  : true
+	]
+	sendEvent(event)
+}
+
+def stop() {
+	log.trace "stop()"
+}
+
+def vidOn() {
+	log.trace "on()"
+	// no-op
+}
+
+def vidOff() {
+	log.trace "off()"
+	// no-op
+}
+
+def installed(){
+	configure()
+}
+
+def updated(){
+	configure()
+}
+
+def configure(){
+	sendEvent(name:"switch2", value:"on") 
+}
+
+def getInHomeURL() {
+   [InHomeURL: cameraRTSP]
 }
